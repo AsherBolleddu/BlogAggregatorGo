42565940a6f8397cba02c975acbeaa4002e0e131
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2025-11-25 20:01:32 -0600

    Implement user registration and database integration with PostgreSQL

--- handler_user.go
+++ handler_user.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/AsherBolleddu/blogaggregator/internal/database"
+	"github.com/google/uuid"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
@@ -14,5 +21,21 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	newUser := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.Args[0],
+	}
+	s.db.CreateUser(context.Background(), newUser)
+
 	return nil
 }
